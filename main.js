532a88b49b9e742d6b93cbfffcc2ed86f6fed667
Philipp Zeuke <it.zeuke@gmail.com>
2022-07-25 23:33:24 +0200

    v1

---
+++ main.js
@@ -0,0 +1,26 @@
+let timer_runs = false;
+let alarm = new Audio("Eieruhr_alarm.mp3");
+
+function start_timer(){
+    if(!timer_runs){
+        timer_runs = true;
+        let start_time = new Date().getTime();
+        const five_minutes = 1000 * 60 * 50;
+        let end_time = start_time + five_minutes;
+    
+        let timer = setInterval(function(){
+            let time_left = end_time - new Date().getTime();
+            if(time_left > 0){
+                let minutes = Math.floor(time_left / (1000 * 60));
+                let seconds = Math.round((time_left / 1000) % 60)
+                if(seconds < 10) seconds = "0" + seconds;
+                time.innerHTML = "0" + minutes + " : " + seconds;
+                console.log(time_left);
+            } else {
+                time.innerHTML = "00 : 00";
+                alarm.play();
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+    }
